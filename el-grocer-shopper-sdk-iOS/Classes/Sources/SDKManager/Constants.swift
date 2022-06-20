f51d8fe3e4c20743c3ff8eb39e7f4cc21d533726
Sarmad Abbas <sarmad@elgrocer.com>
2022-06-20 16:58:52 +0500

    constants moved to new file

---
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/SDKManager/Constants.swift
@@ -0,0 +1,38 @@
+//
+//  Constants.swift
+//  el-grocer-shopper-sdk-iOS
+//
+//  Created by Sarmad Abbas on 20/06/2022.
+//
+
+import Foundation
+
+// MARK: Constants
+
+//private enum BackendSuggestedAction: Int {
+//    case Continue = 0
+//    case ForceUpdate = 1
+//}
+
+let KUpdateBasketToServer = "update To server"
+let kHelpShiftApiKey = "f4b06efaf1612c5925da8888702aeea3"
+let kHelpShiftDomainName = "elgrocer.helpshift.com"
+let kHelpShiftAppId = "elgrocer_platform_20150806182025195-893afc8050f1f9f"
+
+let kHelpshiftChatResponseNotificationKey = "HelpshiftChatResponseNOtification"
+let KRefreshActiveBasketData = "NewBasketRetreiveFromServer"
+let kProductUpdateNotificationKey = "UpdateProductsNotification"
+let kBasketUpdateNotificationKey = "UpdateBasketNotification"
+let kReOrderNotificationKey = "ReOrderNotification"
+let KSlotsUpdate = "slotRefreshCalled"
+let KUpdateGenericSlotView = "slotRefreshView"
+let KCheckPhoneNumber = "slotRefreshCalled"
+let KRefreshGroceries = "Refresh Grocery Called"
+let KGoToMayBasket = "LoadMyBasketVC"
+let KGoBackToOrderScreen = "PendingStageReactiveted"
+let kBasketUpdateForEditNotificationKey = "UpdateBasketForEditNotification"
+let kStartCheckOutProcessKey = "allDataDonenPleaseStartCheckoutProcessFromMYBasketScreen"
+let kGoogleMapsApiKey   =   "AIzaSyA9ItTIGrVXvJASLZXsokP9HEz-jf1PF7c" // forlive
+
+let KGoToBasket = "gotoBackFromTabBar"
+let KCancelOldAllCalls = "CancelDataCalls"
--- el-grocer-shopper-sdk-iOS/Classes/Sources/SDKManager/SDKManager.swift
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/SDKManager/SDKManager.swift
@@ -27,40 +27,6 @@ import FirebaseDynamicLinks
 import FirebaseAuth
 import FirebaseMessaging
 
-private enum BackendSuggestedAction: Int {
-    case Continue = 0
-    case ForceUpdate = 1
-}
-
-let KUpdateBasketToServer = "update To server"
-// MARK: Constants
-let kHelpShiftApiKey = "f4b06efaf1612c5925da8888702aeea3"
-let kHelpShiftDomainName = "elgrocer.helpshift.com"
-let kHelpShiftAppId = "elgrocer_platform_20150806182025195-893afc8050f1f9f"
-
-let kHelpshiftChatResponseNotificationKey = "HelpshiftChatResponseNOtification"
-let KRefreshActiveBasketData = "NewBasketRetreiveFromServer"
-let kProductUpdateNotificationKey = "UpdateProductsNotification"
-let kBasketUpdateNotificationKey = "UpdateBasketNotification"
-let kReOrderNotificationKey = "ReOrderNotification"
-let KSlotsUpdate = "slotRefreshCalled"
-let KUpdateGenericSlotView = "slotRefreshView"
-let KCheckPhoneNumber = "slotRefreshCalled"
-let KRefreshGroceries = "Refresh Grocery Called"
-let KGoToMayBasket = "LoadMyBasketVC"
-let KGoBackToOrderScreen = "PendingStageReactiveted"
-let kBasketUpdateForEditNotificationKey = "UpdateBasketForEditNotification"
-let kStartCheckOutProcessKey = "allDataDonenPleaseStartCheckoutProcessFromMYBasketScreen"
-let kGoogleMapsApiKey   =   "AIzaSyA9ItTIGrVXvJASLZXsokP9HEz-jf1PF7c" // forlive
-
-let KGoToBasket = "gotoBackFromTabBar"
-let KCancelOldAllCalls = "CancelDataCalls"
-
-
-// Notification
-// Force update
-// Sandbird (Fix latter)
-
 open class SDKManager: NSObject, SBDChannelDelegate  {
     
     static var shared: SDKManager = SDKManager()
