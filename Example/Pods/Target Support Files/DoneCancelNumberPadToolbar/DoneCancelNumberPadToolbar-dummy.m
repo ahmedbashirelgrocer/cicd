f9750ae5d27f821d1a08e8eaaf57334d7e081b31
Sarmad Abbas <sarmad@elgrocer.com>
2022-06-23 13:56:44 +0500

    stop tracking of pod

--- Example/Pods/Target Support Files/DoneCancelNumberPadToolbar/DoneCancelNumberPadToolbar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DoneCancelNumberPadToolbar : NSObject
-@end
-@implementation PodsDummy_DoneCancelNumberPadToolbar
-@end
--- Example/Pods/Target Support Files/JDFTooltips/JDFTooltips-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JDFTooltips : NSObject
-@end
-@implementation PodsDummy_JDFTooltips
-@end
