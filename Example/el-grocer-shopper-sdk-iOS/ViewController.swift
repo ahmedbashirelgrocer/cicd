2739a474f1bfe2475310bad700eeaa4cd0906c85
Sarmad Abbas <sarmad@elgrocer.com>
2022-06-29 12:04:37 +0500

    language picker added

--- Example/el-grocer-shopper-sdk-iOS/ViewController.swift
+++ Example/el-grocer-shopper-sdk-iOS/ViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import el_grocer_shopper_sdk_iOS
-import Firebase
 
 class ViewController: UIViewController {
     
@@ -20,7 +19,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPushPayload: UITextField!
     @IBOutlet weak var txtDLPayload: UITextField!
-    @IBOutlet weak var txtLanguage: UITextField!
+    @IBOutlet weak var txtLanguage: UITextField! { didSet {
+        txtLanguage.inputView = languagePicker
+        txtLanguage.addTarget(nil, action: #selector(textFieldDidEndEditing), for: .editingDidEnd)
+    } }
     @IBOutlet weak var btnLaunchSDK: UIButton!{ didSet {
         btnLaunchSDK.backgroundColor = #colorLiteral(red: 0.2550396025, green: 0.2953681946, blue: 0.6989088655, alpha: 1)
         btnLaunchSDK.layer.cornerRadius = 5
@@ -28,24 +30,45 @@ class ViewController: UIViewController {
         btnLaunchSDK.tintColor = .white
     } }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    fileprivate lazy var languagePicker: UIPickerView = {
+        let picker = UIPickerView()
+        picker.delegate = self
+        return picker
+    }()
+    fileprivate lazy var pickerData: [String] = { ["English", "Arabic"] }()
     
     @IBAction func btnGoToSDK(_ sender: Any) {
-        
         self.startSDK()
-        
     }
-    
     
     @objc func startSDK() {
         // ElGrocer.startEngine()
         SDKManager.shared.start()
+    }
+}
+
+//MARK: - Picker View Delegate, Data Source
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        txtLanguage.toolbarPlaceholder = pickerData[row]
+    }
+}
+
+//MARK: - Text Field
+extension ViewController: UITextFieldDelegate {
+    @objc func textFieldDidEndEditing() {
+        txtLanguage.text = pickerData[languagePicker.selectedRow(inComponent: 0)]
     }
 }
