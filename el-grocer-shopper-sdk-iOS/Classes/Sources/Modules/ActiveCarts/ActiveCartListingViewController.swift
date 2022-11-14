45f33c7adf534e2f9d54c6dcaed9d0fb6de1235f
Rashid Khan <rashidkhan@elgrocer.com>
2022-11-14 13:23:11 +0500

    cart listing view controller added.

---
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/ActiveCarts/ActiveCartListingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ActiveCartListingViewController.swift
+//  Adyen
+//
+//  Created by Rashid Khan on 14/11/2022.
+//
+
+import UIKit
+
+class ActiveCartListingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
