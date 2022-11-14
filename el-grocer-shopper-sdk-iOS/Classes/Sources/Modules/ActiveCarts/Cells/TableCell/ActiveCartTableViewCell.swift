e6fbd07169a3dff7477dcd1e9fcca565b26de5ae
Rashid Khan <rashidkhan@elgrocer.com>
2022-11-14 13:56:32 +0500

    cart cell files added.

---
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/ActiveCarts/Cells/TableCell/ActiveCartTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ActiveCartTableViewCell.swift
+//  Adyen
+//
+//  Created by Rashid Khan on 14/11/2022.
+//
+
+import UIKit
+
+class ActiveCartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
