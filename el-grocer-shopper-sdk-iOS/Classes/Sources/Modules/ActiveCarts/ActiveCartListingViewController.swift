15e509e557a5f89453fc0bb8ce6b9127a4329d24
Rashid Khan <rashidkhan@elgrocer.com>
2022-11-14 13:41:05 +0500

    view model type protocol and making logic of controller added.

--- el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/ActiveCarts/ActiveCartListingViewController.swift
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/ActiveCarts/ActiveCartListingViewController.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 class ActiveCartListingViewController: UIViewController {
-
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var viewModel: ActiveCartListingViewModel!
+    
+    static func make(viewModel: ActiveCartListingViewModel) -> ActiveCartListingViewController {
+        let vc = ActiveCartListingViewController(nibName: "ActiveCartListingViewController", bundle: nil)
+        vc.viewModel = viewModel
+        return vc
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func closeButtonTap(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
 }
---
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/ActiveCarts/ActiveCartListingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  ActiveCartListingViewModel.swift
+//  Adyen
+//
+//  Created by Rashid Khan on 14/11/2022.
+//
+
+import Foundation
+
+protocol ActiveCartListingViewModel { }
