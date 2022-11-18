ff172e255f0cd66c08a18933ca2aa0e7475971a6
Rashid Khan <rashidkhan@elgrocer.com>
2022-11-18 15:24:18 +0500

    in progress

--- el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/ActiveCarts/Banner/BannerView.swift
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/ActiveCarts/Banner/BannerView.swift
@@ -10,10 +10,20 @@ import UIKit
 struct BannerDTO: Codable { }
 
 class BannerView: UIView {
-    
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func awakeFromNib() {
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+}
+
+extension BannerView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let collectionView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
     }
 }
