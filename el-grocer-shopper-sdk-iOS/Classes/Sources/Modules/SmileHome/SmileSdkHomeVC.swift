db016002c180d7fce51688da44c1c7db5ff4de6a
Rashid Khan <rashidkhan@elgrocer.com>
2022-12-01 14:10:55 +0500

    cart button red dot logic reverted.

--- el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/SmileHome/SmileSdkHomeVC.swift
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/SmileHome/SmileSdkHomeVC.swift
@@ -450,8 +450,7 @@ class SmileSdkHomeVC: BasketBasicViewController {
     
         self.refreshBasketIconStatus()
         
-        let isCartEmpty = ShoppingBasketItem.getBasketProductsForActiveGroceryBasket(DatabaseHelper.sharedInstance.mainManagedObjectContext).count > 0
-        (self.navigationController as? ElGrocerNavigationController)?.setCartButtonState(isCartEmpty)
+        (self.navigationController as? ElGrocerNavigationController)?.setCartButtonState( ElGrocerUtility.sharedInstance.activeGrocery != nil && ElGrocerUtility.sharedInstance.lastItemsCount > 0)
     }
     
         // MARK: - ButtonAction
