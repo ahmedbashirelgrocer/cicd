8c88cfcd60053616faee0b73bfb97a1aca489e47
ABM <abubaker@elgrocer.com>
2023-03-10 12:26:16 +0500

    update pod Spec

--- el-grocer-shopper-sdk-iOS/Classes/Sources/Utilities/ElGrocerUtility.swift
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Utilities/ElGrocerUtility.swift
@@ -1683,8 +1683,8 @@ extension UIView {
         let gradient: CAGradientLayer = CAGradientLayer()
         gradient.colors = [topColor,bottomColor]
         gradient.locations = [0.0 , 1.0]
-        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradient.endPoint = CGPoint(x: 0.15, y: 0.5)
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 1, y: 0.25)
         gradient.frame = CGRect(x: 0.0, y: -10.0, width: ScreenSize.SCREEN_WIDTH, height: height + 10)
         return gradient
     }
