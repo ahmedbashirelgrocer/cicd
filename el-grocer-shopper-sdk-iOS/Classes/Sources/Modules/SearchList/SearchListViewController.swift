dba003d0a1147370f4e709e643cb45573a164eeb
ABM <abubaker@elgrocer.com>
2023-05-25 18:39:57 +0500

    search list Vc

--- el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/SearchList/SearchListViewController.swift
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/SearchList/SearchListViewController.swift
@@ -131,17 +131,10 @@ class SearchListViewController: UIViewController , NoStoreViewDelegate ,UIScroll
             if isFromHeader{
                 self.addRightCrossButton(sdkManager.isShopperApp)
             }
-        }else{
-//            (self.navigationController as? ElGrocerNavigationController)?.setWhiteBackgroundColor()
+        } else {
+//          (self.navigationController as? ElGrocerNavigationController)?.setWhiteBackgroundColor()
         }
-        
-//        (self.navigationController as? ElGrocerNavigationController)?.actiondelegate = self
-        
-       // self.searchProductListingTextView.inputView = self.searchButton
-        
         FireBaseEventsLogger.setScreenName( FireBaseScreenName.ShoppingList.rawValue , screenClass: String(describing: self.classForCoder))
-        
-       
     }
     
     override func rightBackButtonClicked() {
