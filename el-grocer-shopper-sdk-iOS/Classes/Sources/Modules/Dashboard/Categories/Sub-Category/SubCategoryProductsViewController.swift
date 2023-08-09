a10c1f1f9d80e597d4664a2cba2f66e441c21eee
Rashid Khan <rashidkhan@elgrocer.com>
2023-08-09 15:42:37 +0500

    sub-category view controller and view model created.

---
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/Dashboard/Categories/Sub-Category/SubCategoryProductsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SubCategoryProductsViewController.swift
+//  el-grocer-shopper-sdk-iOS
+//
+//  Created by Rashid Khan on 09/08/2023.
+//
+
+import UIKit
+
+class SubCategoryProductsViewController: UIViewController {
+    private var viewModel: SubCategoryProductsViewModelType!
+
+    static func make(viewModel: SubCategoryProductsViewModelType) -> SubCategoryProductsViewController {
+        let vc = SubCategoryProductsViewController(nibName: "SubCategoryProductsViewController", bundle: .resource)
+        vc.viewModel = viewModel
+        return vc
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
---
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/Dashboard/Categories/Sub-Category/SubCategoryProductsViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  SubCategoryProductsViewModel.swift
+//  el-grocer-shopper-sdk-iOS
+//
+//  Created by Rashid Khan on 09/08/2023.
+//
+
+import Foundation
+
+protocol SubCategoryProductsViewModelInputs { }
+
+protocol SubCategoryProductsViewModelOutputs { }
+
+protocol SubCategoryProductsViewModelType: SubCategoryProductsViewModelInputs, SubCategoryProductsViewModelOutputs {
+    var inputs: SubCategoryProductsViewModelInputs { get }
+    var outputs: SubCategoryProductsViewModelOutputs { get }
+}
+
+extension SubCategoryProductsViewModelType {
+    var inputs: SubCategoryProductsViewModelInputs { self }
+    var outputs: SubCategoryProductsViewModelOutputs { self }
+}
+
+class SubCategoryProductsViewModel: SubCategoryProductsViewModelType {
+    
+}
