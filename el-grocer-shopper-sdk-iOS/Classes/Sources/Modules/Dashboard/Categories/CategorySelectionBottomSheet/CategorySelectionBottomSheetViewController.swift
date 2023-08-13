1cb7717db5c4b94729f8345b81b760c445acada9
Rashid Khan <rashidkhan@elgrocer.com>
2023-08-14 01:49:47 +0500

    category selection bottom sheet implemented

---
+++ el-grocer-shopper-sdk-iOS/Classes/Sources/Modules/Dashboard/Categories/CategorySelectionBottomSheet/CategorySelectionBottomSheetViewController.swift
@@ -0,0 +1,64 @@
+//
+//  CategorySelectionBottomSheetViewController.swift
+//  el-grocer-shopper-sdk-iOS
+//
+//  Created by Rashid Khan on 13/08/2023.
+//
+
+import UIKit
+import RxDataSources
+import RxSwift
+import RxCocoa
+
+class CategorySelectionBottomSheetViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var viewModel: CategorySelectionViewModelType!
+    private var dataSource: RxCollectionViewSectionedReloadDataSource<SectionModel<Int, ReusableCollectionViewCellViewModelType>>!
+    private var disposeBag = DisposeBag()
+    
+    static func make(viewModel: CategorySelectionViewModelType) -> CategorySelectionBottomSheetViewController {
+        let vc = CategorySelectionBottomSheetViewController(nibName: "CategorySelectionBottomSheetViewController", bundle: .resource)
+        vc.viewModel = viewModel
+        return vc
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+        bindViews()
+    }
+}
+
+fileprivate extension CategorySelectionBottomSheetViewController {
+    func setupViews() {
+        
+        self.collectionView.register(UINib(nibName: StoresCategoriesCollectionViewCell.defaultIdentifier, bundle: .resource), forCellWithReuseIdentifier: StoresCategoriesCollectionViewCell.defaultIdentifier)
+        
+        self.collectionView.collectionViewLayout = {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .vertical
+            layout.itemSize = CGSize(width: (ScreenSize.SCREEN_WIDTH - 80) / 4, height: 110)
+            layout.minimumInteritemSpacing = 16
+            layout.minimumLineSpacing = 16
+            let edgeInset:CGFloat =  16
+            layout.sectionInset = UIEdgeInsets(top: edgeInset / 2, left: edgeInset, bottom: 0, right: edgeInset)
+            return layout
+        }()
+    }
+    
+    func bindViews() {
+        self.dataSource = RxCollectionViewSectionedReloadDataSource(configureCell: { _, collectionView, indexPath, viewModel in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: viewModel.reusableIdentifier, for: indexPath) as! RxUICollectionViewCell
+            cell.configure(viewModel: viewModel)
+            return cell
+        })
+        
+        self.viewModel.outputs.categoriesDataSource
+            .bind(to: self.collectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+        
+        self.collectionView.rx.modelSelected(StoresCategoriesCollectionViewCellViewModel.self)
+    }
+}
